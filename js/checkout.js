67d4f781aa7b9708512a5c9d873d23f90836e8ca
MarinaUG <marina.unamuno@gmail.com>
2023-03-03 18:03:12 +0100

    SprintSolution 1

--- js/checkout.js
+++ js/checkout.js
@@ -1,28 +1,99 @@
+// Exercise 7
 
-// Exercise 6
 function validate() {
 	var error = 0;
 	// Get the input fields
 	var fName = document.getElementById("fName");
 	var fEmail = document.getElementById("fEmail");
+	var fAddress = document.getElementById("fAddress");
+	var fLastN = document.getElementById("fLastN");
+	var fPassword = document.getElementById("fPassword");
+	var fPhone = document.getElementById("fPhone");
 
 	// Get the error elements
 	var errorName = document.getElementById("errorName");
-	var errorEmail = document.getElementById("errorEmail");  
+	var errorEmail = document.getElementById("errorEmail");
+	var errorPhone = document.getElementById("errorPhone");
+	var errorLastN = document.getElementById("errorLastN");
+	var errorPassword = document.getElementById("errorPassword");
+	var errorAdress = document.getElementById("errorAddress");
+
+	let regExpName=/([A-Z])\w+/g;
+	let regExpLastN=/([A-Z])\w+/g;
+	let regExpEmail= /\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/gi;
+	let regExpPhone=/\d/g;  
+	let regExpAdress=/./g;
+	let regExpPassword=/^[A-Za-z0-9]*$/g;
+	
+	let expMail=regExpEmail.test(fEmail.value);
 	
 	// Validate fields entered by the user: name, phone, password, and email
-	if(fName.value == ""){
-		error++;
-	}
+	
+		if (fName.value.length<4 || !(regExpName.test(fName.value))) {
+			error++;
+			fName.style.border="2px solid red";
+			errorName.innerHTML = "This field is required and must have, at least, 3 characters only letters";
+			errorName.style.display="block";
+		}
+		else{
+			fName.style.border="2px solid green";
+		}
 
-	if(fEmail.value == ""){
-		error++;
-	}
-	 
-	if(error>0){
+		if (fLastN.value.length<4 || !(regExpLastN.test(fLastN.value))) {
+			error++;
+			fLastN.style.border="2px solid red";
+			errorLastN.innerHTML = "This field is required and must have, at least, 3 characters only letters";
+			errorLastN.style.display="block";
+		}
+		else{
+			fLastN.style.border="2px solid green";
+		}
+
+		if (fAddress.value.length<4 || !(regExpAdress.test(fAddress.value))) {
+			error++;
+			fAddress.style.border="2px solid red";
+			errorAdress.innerHTML = "This field is required and must have, at least, 3 characters";
+			errorAdress.style.display="block";
+		}
+		else{
+			fAddress.style.border="2px solid green";
+		}
+		
+		if ( !(expMail) || fEmail.length<4) {
+			error++;
+			fEmail.style.border="2px solid red";
+			errorEmail.innerHTML = "This field is required and must contain an '@' and have, at least, 3 characters";
+			errorEmail.style.display="block";
+		}
+		else{
+			fEmail.style.border="2px solid green";
+		}
+
+		if (!(regExpPhone.test(fPhone.value)) || fPhone.value.length != 9 ) {
+			error++;
+			fPhone.style.border="2px solid red";
+			errorPhone.innerHTML = "Invalid phone number!! Must be 9 digits with no letters";
+			errorPhone.style.display="block";
+		}
+		else{
+			fPhone.style.border="2px solid green";
+		}
+		
+		console.log(fPassword.value);
+		if ( fPassword.value.length<4 || !(regExpPassword.test(fPassword.value))) {				
+			error++;
+			fPassword.style.border="2px solid red";
+			errorPassword.innerHTML = "Enter a correct password,4-8 numbers o letters";
+			errorPassword.style.display="block";
+		}
+		else{
+			fPassword.style.border="2px solid green";
+		}
+	
+	if (error > 0) {
 		alert("Error");
-	}else{
+	} else {
 		alert("OK");
 	}
-
+	
 }
--- js/shop.js
+++ js/shop.js
@@ -1,6 +1,5 @@
 // If you have time, you can move this variable "products" to a json or js file and load the data in this js. It will look more professional
-var products = [
-   {
+var products = [{
         id: 1,
         name: 'cooking oil',
         price: 10.5,
@@ -68,58 +67,107 @@ var cartList = [];
 
 // Improved version of cartList. Cart is an array of products (objects), but each one has a quantity field to define its quantity, so these products are not repeated.
 var cart = [];
-
 var total = 0;
 
 // Exercise 1
 function buy(id) {
     // 1. Loop for to the array products to get the item to add to cart
+    let trobat = false;
+    let i = 0;
+    while (i < products.length && !trobat) {
+        if (id == products[i].id) {
+            trobat = true;
+        }
+        i++;
+    }
     // 2. Add found product to the cartList array
+    cartList.push(products[i - 1]);
+    console.log(cartList);
 }
 
 // Exercise 2
 function cleanCart() {
-
+    cartList.length = 0;
+    console.log(cartList);
 }
 
 // Exercise 3
 function calculateTotal() {
     // Calculate total price of the cart using the "cartList" array
+    let i = 0;
+    let sum = 0;
+    for (i = 0; i < cartList.length; i++) {
+        sum = sum + cartList[i].price;
+    }
+    console.log(sum);
+    return sum;
 }
 
 // Exercise 4
-function generateCart() {
+function generateCart(cartList) {
     // Using the "cartlist" array that contains all the items in the shopping cart, 
     // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
+
+    cart = [...new Set(cartList)]; //crea un array nuevo con los elementos no repetidos
+    for (i = 0; i < cart.length; i++) {
+        cart[i].quantity = 0;
+    }
+    for (i = 0; i < cart.length; i++) {
+        for (j = 0; j < cartList.length; j++) {
+            if (cart[i].id === cartList[j].id) {
+                cart[i].quantity = cart[i].quantity + 1;
+            }
+        }
+    }
+    console.log(cart);
 }
 
 // Exercise 5
-function applyPromotionsCart() {
+function applyPromotionsCart(cart) {
     // Apply promotions to each item in the array "cart"
+   
+    for (i = 0; i < cart.length; i++) {
+        if (cart[i].id = 1 && cart[i].quantity > 2) {
+            cart[i].price = 10;
+            console.log(cart);
+        }
+        if (cart[i].id = 3 && cart[i].quantity > 9) {
+            cart[i].price = parseFloat(cart[i].price * (1 / 3)).toFixed(2);
+            console.log(cart);
+        }
+    } 
 }
 
 // Exercise 6
 function printCart() {
     // Fill the shopping cart modal manipulating the shopping cart dom
-}
-
+    applyPromotionsCart(cart);
+    let cartItems=document.getElementById("cart_list");
+    let totalPrice=document.getElementById("total_price");
+    let totalProduct=[];
+    for(i=0; i<cart.length;i++){
+        totalProduct[i]=cart[i].price*cart[i].quantity;
+        cartItems.innerHTML+= '<th scope="row">'+cart[i].name+'</th><td>$'+cart[i].price+'</td><td>'+cart[i].quantity+'</td><td>$'+totalProduct[i]+'</td>'
+    }
+    totalPrice.innerHTML=calculateTotal();
+} 
 
 // ** Nivell II **
 
-// Exercise 7
+// Exercise 
 function addToCart(id) {
     // Refactor previous code in order to simplify it 
     // 1. Loop for to the array products to get the item to add to cart
     // 2. Add found product to the cart array or update its quantity in case it has been added previously.
 }
 
-// Exercise 8
+// Exercise 9
 function removeFromCart(id) {
     // 1. Loop for to the array products to get the item to add to cart
     // 2. Add found product to the cartList array
 }
 
-function open_modal(){
-	console.log("Open Modal");
-	printCart();
+function open_modal() {
+    console.log("Open Modal");
+    printCart();
 }
